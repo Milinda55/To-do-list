8d2ad5b67eab4212f081258d107e90e176065172
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-03-04 11:46:01 +0530

    Implement logic to prepend tasks on input and add function to handle task insertion dynamically

--- src/js/main.js
+++ src/js/main.js
@@ -7,13 +7,36 @@ import $ from 'jquery';
 //     $("#completed-task-list").append(taskItem.clone());
 // }
 
+const taskLists = [];
+
+class TaskItem {
+    id;
+    description;
+    status
+
+    constructor(id, description, status) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+    }
+}
+
 $("#frm-task").on("submit", ()=> {
     const textDescription = $("#txt-task").val().trim();
     $("#tasks-list #no-task").hide();
-    const newTask = $(`
-    <div class="task-item d-flex justify-content-between p-2  align-items-center rounded-2 text-secondary-emphasis">-->
+    const taskId = taskLists.length;
+    taskLists.push(new TaskItem(taskId, textDescription));
+
+});
+
+function renderTasks() {
+    for (const {id, description, status} of taskLists) {
+        const task = `
+            <div class="task-item d-flex justify-content-between p-2  align-items-center rounded-2 text-secondary-emphasis">
               <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <input class="form-check-input" type="checkbox" 
+                value="" id="task-${id}">
+                
                 <label class="form-check-label" for="flexCheckDefault">
                   Default checkbox
                 </label>
@@ -23,7 +46,6 @@ $("#frm-task").on("submit", ()=> {
                 <i class="bi bi-trash" title="Delete"></i>
               </div>
             </div>
-    `);
-    $("#tasks-list > section").prepend(newTask);
-
-});
+            `
+    }
+}
