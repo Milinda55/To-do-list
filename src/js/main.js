d675110ad20a8fa89af70db2212df7a442ae1089
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-03-04 11:19:11 +0530

    Removed dummy data and implemented 'No Tasks' placeholder

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 import $ from 'jquery';
 
-const taskItem = $(".task-item");
-
-for (let i = 0; i < 5; i++) {
-    $("#tasks-list").append(taskItem.clone());
-    $("#completed-task-list").append(taskItem.clone());
-}
+// const taskItem = $(".task-item").remove();
+//
+// for (let i = 0; i < 5; i++) {
+//     $("#tasks-list").append(taskItem.clone());
+//     $("#completed-task-list").append(taskItem.clone());
+// }
